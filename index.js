c8581de9a62a01caeaac1ee11efb9fd8a654c70f
Cchris7 <85213942+Cchris7@users.noreply.github.com>
2023-04-09 23:02:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,29 @@ app.get('/rest/ticket/:id', (req,res) => {
     res.json(search);
 });
 
+app.post('/rest/ticket' , (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./tickets.json'));
+    
+    const newTicket = {
+        id: req.body.id,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at,
+    type: req.body.type,
+    subject: req.body.subject,
+    description: req.body.description,
+    priority: req.body.priority,
+    status: req.body.status,
+    recipient: req.body.recipient,
+    submitter: req.body.submitter,
+    assignee_id: req.body.assignee_id,
+    follower_ids: req.body.follower_ids,
+    tags: req.body.tags,
+  };
+      
+  data.push(newTicket);
+  fs.writeFileSync('./mydata.json', JSON.stringify(data));
+  res,json(newTicket);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
